3be177f71d174edcc2950c93835143420741828c
aandis97 <achandis97@gmail.com>
2021-12-05 07:18:52 +0700

    add base layout and template

---
+++ resources/views/admin/templates/components/_sidebar.blade.php
@@ -0,0 +1,21 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+    <!-- /.search form -->
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu" data-widget="tree">
+      <li class="header">MAIN NAVIGATION</li>
+      <li >
+        <a href="#">
+          <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          <span class="pull-right-container">
+          </span>
+        </a>
+      </li>
+      <li class=""><a href=""><i class="fa fa-circle-o text-aqua"></i> <span>Category</span></a></li>
+      <li class=""><a href=""><i class="fa fa-circle-o text-aqua"></i> <span>Product</span></a></li> 
+    </ul>
+  </section>
+  <!-- /.sidebar -->
+</aside>
